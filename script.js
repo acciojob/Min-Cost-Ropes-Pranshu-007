053e0fdf8d3a152ea03a0d8fa45f4ed17552f7b4
Pranshu-007 <84780774+Pranshu-007@users.noreply.github.com>
2025-02-18 15:51:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if (arr.length <= 1) return 0;
+
+  arr.sort((a, b) => a - b);
+  let totalCost = 0;
+
+  while (arr.length > 1) {
+    const [first, second] = arr.splice(0, 2);
+    const cost = first + second;
+    totalCost += cost;
+    console.log(totalCost)
+
+    arr.push(cost);
+    arr.sort((a, b) => a - b);
+  }
+  return totalCost;
   
 }
 
